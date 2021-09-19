1e6b3b1e270a03aa76e7bc2f76e9901af30cddea
AirMaxSys <gy520813@live.com>
2021-09-19 14:58:39 +0800

    add set FreeRTOS directories script

--- script/set_env.sh
+++ script/set_env.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "export FREERTOS_DIR_ENV=" >> ~/.bashrc
-echo "export FREERTOS_KERNEL_DIR_ENV=" >> ~/.bashrc
-$(source ~/.bashrc)
+CURRENT_DIR="$(dirname ${0})"
+FREERTOS_REL_DIR="${CURRENT_DIR}/../FreeRTOS"
+FREERTOS_DIR="$(realpath ${FREERTOS_REL_DIR})"
+FREERTOS_KERNEL_DIR="${FREERTOS_DIR}/FreeRTOS/Source"
+
+echo "export FREERTOS_DIR_ENV=${FREERTOS_DIR}" >> ~/.bashrc
+echo "export FREERTOS_KERNEL_DIR_ENV=${FREERTOS_KERNEL_DIR}" >> ~/.bashrc
+
+source ~/.bashrc
 
