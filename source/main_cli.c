98d9af6980a84b13bb6ef0c224b5fc707e6962b3
AirMaxSys <gy520813@live.com>
2021-09-21 18:42:40 +0800

    fix command handle logic error

--- source/main_cli.c
+++ source/main_cli.c
@@ -7,9 +7,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
-#include "console.h"
-
 #include "FreeRTOS_CLI.h"
+#include "console.h"
 
 #define CLI_PRIORITY    100
 #define CLI_STACK_SIZE  1024
@@ -43,17 +42,18 @@ static void vConsoleInterpreterTask(void *para)
 {
     char ch = 0;
     int idx = 0;
-    ssize_t ret = 0;
+    ssize_t read_ret = 0;
+    BaseType_t xReturn = pdTRUE;
     sigset_t prev_mask, all_mask;
-    const char * const pcCommandInput = cCliInputBuf;
+    const char * pcCommandInput = cCliInputBuf;
     char * pcWriteBuffer = cCliOutputBuf;
     size_t xWriteBufferLen = CLI_OP_BUF_LEN;
 
-    // Init mask all signal set
+    // Init signal set and mask all
     sigfillset(&all_mask);
 
     for (;;) {
-        // output "#"
+        // output #
         putchar('#');
         fflush(stdout);
 
@@ -68,7 +68,7 @@ static void vConsoleInterpreterTask(void *para)
         }
 #endif
         // Get input command
-        while ((ret = read(STDIN_FILENO, &ch, 1)) != 0) {
+        while ((read_ret = read(STDIN_FILENO, &ch, 1)) != 0) {
             if (ch != '\n' && ch != '\0') {
                 cCliInputBuf[idx] = ch;
                 //putchar(ch);
@@ -81,9 +81,9 @@ static void vConsoleInterpreterTask(void *para)
                 continue;
             }
         }
-        if (ret != 0) {
+        if (read_ret < 0) {
             fprintf(stderr, "ERRNO[%d]:%s\n", errno, strerror(errno));
-            ret = 0;
+            read_ret = 0;
         }
 
         // Restore previous masked signal set
@@ -92,13 +92,17 @@ static void vConsoleInterpreterTask(void *para)
 
         // printf("idx:%d str:%s\n", idx, cCliInputBuf);
 
-        // Interpret input command by CLI interpreter
-        FreeRTOS_CLIProcessCommand(pcCommandInput, pcWriteBuffer, xWriteBufferLen);
-        // Print output of command
-        fprintf(stdout, "%s", pcWriteBuffer);
-        // restor pointer and counter
+        // Interpret input command unitll without False
+        // Print interpretor handl result
+        do {
+            xReturn = FreeRTOS_CLIProcessCommand(pcCommandInput, pcWriteBuffer, xWriteBufferLen);
+            fprintf(stdout, "%s", pcWriteBuffer);
+        } while (xReturn != pdFALSE);
+
+        // restore pointer and counter
         idx = 0;
         pcWriteBuffer = cCliOutputBuf;
+        pcCommandInput = cCliInputBuf;
     }
 }
 
